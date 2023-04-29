f043010f05afe13082087691325c9e4db5bd4efb
Ritesh <riteshnahavre2909@gmail.com>
2023-04-29 23:47:14 +0530

    test3

--- src/authContext/apiCalls.js
+++ src/authContext/apiCalls.js
@@ -4,7 +4,7 @@ import { axiosInstance } from "../config";
 export const login = async (user,dispatch)=>{
     dispatch(loginStart());
     try {
-        const res = await axiosInstance.post("auth/login", {mode:'no-cors'},user);
+        const res = await axiosInstance.post("auth/login",user);
         dispatch(loginSuccess(res.data));
     } catch (err) {
             dispatch(loginFailure());
